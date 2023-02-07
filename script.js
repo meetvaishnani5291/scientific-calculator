32fb3d73f3515089df8c99611a08cc790ff6c7fc
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-02-07 19:12:09 +0530

    frequently used functions implemented

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ let parenthesisCloseBtn = document.querySelector(".parenthesis-close");
 let equaltoBtn = document.querySelector(".equalto");
 let clearBtn = document.querySelector(".CE");
 let backspaceBtn = document.querySelector(".back-space");
-
 let plusOrMinusBtn = document.querySelector(".plus-or-minus");
 let factorialBtn = document.querySelector(".factorial");
-let piBtn = document.querySelector(".const-PI");
-let eBtn = document.querySelector(".const-E");
+let piBtn = document.querySelector(".const-pi");
+let eBtn = document.querySelector(".const-e");
 let squareBtn = document.querySelector(".square");
 let inverseBtn = document.querySelector(".inverse");
 let absBtn = document.querySelector(".abs");
@@ -22,7 +21,8 @@ let logBtn = document.querySelector(".log");
 let tenpowerBtn = document.querySelector(".ten-power");
 let xPowerBtn = document.querySelector(".x-power");
 let underootBtn = document.querySelector(".underoot");
-
+let secondBtn = document.querySelector(".second-btn");
+let alternativeFunctionsBtns = document.querySelectorAll(".second");
 
 let evaluate = function () {
   try {
@@ -30,6 +30,7 @@ let evaluate = function () {
       return;
     }
     let temp = currentOperation.value;
+    currentOperation.value = currentOperation.value.replaceAll("^", "**");
     currentOperation.value = eval(currentOperation.value);
     previousOperation.value = temp;
   } catch (err) {
@@ -40,7 +41,6 @@ let clearInput = function () {
   currentOperation.value = "";
 };
 let removeLastSymbol = function () {
-  console.log("clicked");
   currentOperation.value = currentOperation.value.slice(0, -1);
 };
 let appendInput = function (event) {
@@ -99,17 +99,156 @@ equaltoBtn.addEventListener("click", evaluate);
 clearBtn.addEventListener("click", clearInput);
 backspaceBtn.addEventListener("click", removeLastSymbol);
 
-// plusOrMinusBtn.addEventListener("click", parenthesisOpen);
-// factorialBtn.addEventListener("click", parenthesisOpen);
-// piBtn.addEventListener("click", parenthesisOpen);
-// eBtn.addEventListener("click", parenthesisOpen);
-// squareBtn.addEventListener("click", parenthesisOpen);
-// inverseBtn.addEventListener("click", parenthesisOpen);
-// absBtn.addEventListener("click", parenthesisOpen);
-// expBtn.addEventListener("click", parenthesisOpen);
-// modBtn.addEventListener("click", parenthesisOpen);
-// lnBtn.addEventListener("click", parenthesisOpen);
-// logBtn.addEventListener("click", parenthesisOpen);
-// tenpowerBtn.addEventListener("click", parenthesisOpen);
-// xPowerBtn.addEventListener("click", parenthesisOpen);
-// underootBtn.addEventListener("click", parenthesisOpen);
+plusOrMinusBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = -1 * currentOperation.value;
+    previousOperation.value = "-(" + temp + ")";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+
+// factorialBtn.addEventListener(
+//     "click",
+//     (event) => {
+//         try{
+//             let temp = currentOperation.value;
+//             currentOperation.value = -1 * currentOperation.value;
+//             previousOperation.value = temp;
+
+//         }catch(err){
+//             currentOperation.value = "Invalid syntax";
+//         }
+//     }
+// );
+piBtn.addEventListener("click", (event) => {
+  try {
+    currentOperation.value = Math.PI;
+    previousOperation.value = "PI";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+eBtn.addEventListener("click", (event) => {
+  try {
+    currentOperation.value = Math.E;
+    previousOperation.value = "E";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+squareBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = currentOperation.value ** 2;
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = temp;
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+inverseBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = 1 / currentOperation.value;
+    previousOperation.value = "1/" + temp;
+    if (isNaN(currentOperation.value)) throw "Not a number";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+absBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = Math.abs(currentOperation.value);
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = "abs(" + temp + ")";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+expBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = Math.exp(currentOperation.value);
+    previousOperation.value = temp;
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+modBtn.addEventListener("click", (event) => {
+  try {
+    if (!currentOperation.value.length) {
+      return;
+    }
+    if (
+      !isNaN(currentOperation.value.slice(-1)) ||
+      currentOperation.value.slice(-1) == ")"
+    ) {
+      currentOperation.value += "%";
+    }
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+lnBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = Math.log(currentOperation.value);
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = "ln(" + temp + ")";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+logBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = Math.log10(currentOperation.value);
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = "log(" + temp + ")";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+tenpowerBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = 10 ** currentOperation.value;
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = "10^" + temp;
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+xPowerBtn.addEventListener("click", (event) => {
+  try {
+    if (!currentOperation.value.length) {
+      return;
+    }
+    if (
+      !isNaN(currentOperation.value.slice(-1)) ||
+      currentOperation.value.slice(-1) == ")"
+    ) {
+      currentOperation.value += "^";
+    }
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+underootBtn.addEventListener("click", (event) => {
+  try {
+    let temp = currentOperation.value;
+    currentOperation.value = Math.sqrt(currentOperation.value);
+    if (isNaN(currentOperation.value)) throw "Not a number";
+    previousOperation.value = "sqrt(" + temp + ")";
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+});
+secondBtn.addEventListener("click", () => {
+  alternativeFunctionsBtns.forEach((button) => {
+    button.classList.toggle("hide-btn");
+  });
+});
