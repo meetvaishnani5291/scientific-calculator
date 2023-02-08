612ce0e7a0e03cbea8f5ead4840ab75e345330a4
meetvaishnani5291 <meet.vaishnani@simformsolutions.com>
2023-02-09 00:24:55 +0530

    trigonometry functions optimized

--- script.js
+++ script.js
@@ -429,7 +429,8 @@ mstoreBtn.addEventListener("click", () => {
 //  function for calcution of all trigonomerty functions
 let trigonometryCalculate = function (event) {
   let inputNumber = currentOperation.value;
-  let trigonometryOpeartion = event.target.id;
+  let trigonometryOpeartion = event.currentTarget.id;
+  console.log(trigonometryOpeartion);
   try {
     let degreeForFunction = degreeFlag
       ? (currentOperation.value * Math.PI) / 180
@@ -439,23 +440,71 @@ let trigonometryCalculate = function (event) {
         currentOperation.value
       );
       if (trigonometryOpeartion[0] == "a") {
-        previousOperation.value = trigonometryOpeartion + "(" + inputNumber + ")";
+        previousOperation.value =
+          trigonometryOpeartion.slice(1) + "I(" + inputNumber + ")";
       } else {
-        previousOperation.value = "sinh(" + inputNumber + ")";
+        previousOperation.value =
+          trigonometryOpeartion.slice(0) + "(" + inputNumber + ")";
       }
-    } else if (trigonometryOpeartion.slice[0] == "a") {
+    } else if (trigonometryOpeartion.slice(0, 1) == "a") {
       currentOperation.value = degreeFlag
         ? (180 / Math.PI) * Math[trigonometryOpeartion](currentOperation.value)
         : Math[trigonometryOpeartion](currentOperation.value);
-        previousOperation.value = "sinh`("+inputNumber + ')';
+      previousOperation.value =
+        trigonometryOpeartion.slice(1) + "I(" + inputNumber + ")";
     } else {
       currentOperation.value = Math[trigonometryOpeartion](degreeForFunction);
+      previousOperation.value = trigonometryOpeartion + "(" + inputNumber + ")";
     }
     if (isNaN(currentOperation.value)) throw "Not a number.";
+    currentOperation.value = Number(currentOperation.value).toFixed(5);
   } catch (err) {
     currentOperation.value = "Invalid syntax";
   }
 };
-document
-  .querySelector(".sin-btn")
-  .addEventListener("click", trigonometryCalculate);
+let trigonometryCalculateForOther = function (event) {
+  let inputNumber = currentOperation.value;
+  let trigonometryOpeartion = event.currentTarget.id;
+  let displayOperation = event.target.textContent.toString();
+  try {
+    let degreeForFunction = degreeFlag
+      ? (currentOperation.value * Math.PI) / 180
+      : currentOperation.value;
+    if (trigonometryOpeartion.slice(-1) == "h") {
+      if (trigonometryOpeartion[0] == "a") {
+        currentOperation.value = Math[trigonometryOpeartion](
+          1 / currentOperation.value
+        );
+        previousOperation.value =
+          displayOperation.slice(0, 4) + "I(" + inputNumber + ")";
+      } else {
+        currentOperation.value =
+          1 / Math[trigonometryOpeartion](currentOperation.value);
+        previousOperation.value =
+          displayOperation.slice(0, 4) + "(" + inputNumber + ")";
+      }
+    } else if (trigonometryOpeartion.slice(0, 1) == "a") {
+      currentOperation.value = degreeFlag
+        ? (180 / Math.PI) *
+          Math[trigonometryOpeartion](1 / currentOperation.value)
+        : Math[trigonometryOpeartion](1 / currentOperation.value);
+      previousOperation.value =
+        displayOperation.slice(0, 3) + "I(" + inputNumber + ")";
+    } else {
+      currentOperation.value =
+        1 / Math[trigonometryOpeartion](degreeForFunction);
+      previousOperation.value =
+        displayOperation.slice(0, 3) + "(" + inputNumber + ")";
+    }
+    if (isNaN(currentOperation.value)) throw "Not a number.";
+    currentOperation.value = Number(currentOperation.value).toFixed(5);
+  } catch (err) {
+    currentOperation.value = "Invalid syntax";
+  }
+};
+document.querySelectorAll(".trigonometry").forEach((button) => {
+  button.addEventListener("click", trigonometryCalculate);
+});
+document.querySelectorAll(".trigonometryOther").forEach((button) => {
+  button.addEventListener("click", trigonometryCalculateForOther);
+});
